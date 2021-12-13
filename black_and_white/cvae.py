e9ea944e8a99a9fc63000c132e9075fba8c06d8d
Raul <s202489@student.dtu.dk>
2021-12-13 11:00:06 +0100

    chore: restructuring

---
+++ black_and_white/cvae.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from utils import SmilesDataset, image_grid
+from models import VAE, CVAE
+from PIL import Image
+
+def main():
+    transform = transforms.Compose(
+                    [transforms.Grayscale(num_output_channels=1),
+                    transforms.Resize((50,50),interpolation=Image.NEAREST),
+                    transforms.ToTensor()])
+
+    dataset = SmilesDataset(csv_file="smiles_dataset.txt", root_dir="filtered", transform=transform)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8)
+    dataloader_eval = torch.utils.data.DataLoader(dataset, batch_size=32)
+    
+    for sample in dataloader:
+        input_shape = sample["image"].shape
+        break
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    cvae = CVAE(input_size=input_shape, latent_size=600).to(device)
+
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(cvae.parameters(), lr=0.00002)
+    train_loss_history = []
+    num_epochs = 1000
+    cvae.train()
+    for epoch in range(num_epochs):
+        train_loss = 0.0
+        for sample in dataloader:
+            optimizer.zero_grad()
+
+            images = sample["image"].to(device)
+            encodings = sample["encode"].to(device)
+
+            outdict = cvae(images, encodings)
+            pz, qz, z, x, xhat = outdict["pz"], outdict["qz"], outdict["z"], outdict["x"], outdict["xhat"]
+            
+            # compute loss
+            reconloss = criterion(xhat, x)
+            MSE = torch.mean((xhat - x)**2)
+
+            KLD = -0.5 * torch.mean(torch.mean(1 + torch.log(qz.sigma**2) - qz.mu**2 - torch.exp(qz.sigma**2)))
+            loss = MSE + KLD
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.detach().cpu().item()
+
+        cvae.eval()
+        for sample in dataloader_eval:
+            optimizer.zero_grad()
+            images = sample["image"].to(device)
+            encodings = sample["encode"].to(device)
+            outdict = cvae(images, encodings)
+            x, xhat = outdict["x"], outdict["xhat"]
+            break
+
+        images = [Image.fromarray(255*img.detach().numpy()[0]).convert("L") for img in xhat]
+        grid = image_grid(images, 4, 8)
+        grid.save(f"results/cvae/iter_{epoch}.png")
+
+        if epoch == 0:
+            images = [Image.fromarray(255*img.detach().numpy()[0]).convert("L") for img in x]
+            grid = image_grid(images, 4, 8)
+            grid.save(f"results/cvae/original.png")
+
+        train_loss_history.append(train_loss)
+
+        torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': cvae.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': train_loss,
+                    }, "model_savepoints/cvae/model.pt")
+
+        print(f"epoch:{epoch}, loss:{train_loss_history[-1]}, mse: {BCE}, kld: {KLD}")
+    
+if __name__ == "__main__":
+    main()
